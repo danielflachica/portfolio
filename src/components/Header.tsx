2b873dd0f96a4700b744a01339b1707d0cd7a134
Daniel Lachica <daniel.lachica82@gmail.com>
2026-07-20 16:28:12 +0800

    Update Header component layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
-import { Heading, HStack } from "@chakra-ui/react";
+import { Box, Heading, HStack } from "@chakra-ui/react";
 import { LuGlobe } from "react-icons/lu";
 import { ColorModeButton } from "@/components/ui/color-mode";
 
 const Header = () => {
   return (
-    <HStack justifyContent="space-between" pl={2} pr={0} py={0}>
-      <LuGlobe />
+    <HStack justifyContent="space-between" p={0}>
+      <Box px={2}>
+        <LuGlobe />
+      </Box>
       <Heading size="sm">Daniel Lachica</Heading>
       <ColorModeButton size="xs" borderRadius={0} />
     </HStack>
