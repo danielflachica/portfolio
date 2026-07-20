ac4dfd153bd4826df8cab78a89884ef3a58d9b0c
Daniel Lachica <daniel.lachica82@gmail.com>
2026-07-20 17:34:23 +0800

    Add typewriter effect to Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,13 +8,26 @@ import {
   Button,
   Flex,
 } from "@chakra-ui/react";
+import Typewriter, { type Options } from "typewriter-effect";
 
 const Hero = () => {
+  const twConfig: Options = {
+    strings: [
+      "I build websites",
+      "I design solutions",
+      "And I\'m open to work!",
+    ],
+    autoStart: true,
+    loop: true,
+    cursor: "_",
+  };
+
   return (
     <Container alignContent="center" bg="bg.emphasized" height="80dvh">
       <Flex px={{ base: 2, md: 10 }}>
         <VStack align={{ base: "center", md: "flex-start" }} gap={8}>
           <Heading size={{ base: "5xl", md: "6xl" }}>Hi! I'm Daniel</Heading>
+          <Typewriter options={twConfig} />
           <Text textAlign={{ base: "center", md: "left" }}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
             pellentesque turpis mi, eu vehicula justo efficitur sed. Nunc et
