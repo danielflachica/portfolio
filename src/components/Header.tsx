af6f67d5e6fb32bdf32641690ef2ef5489dd880c
Daniel Lachica <daniel.lachica82@gmail.com>
2026-07-20 16:26:39 +0800

    Add color switcher component to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import { Heading, HStack } from "@chakra-ui/react";
-import { LuGlobe, LuMoon } from "react-icons/lu";
+import { LuGlobe } from "react-icons/lu";
+import { ColorModeButton } from "@/components/ui/color-mode";
 
 const Header = () => {
   return (
-    <HStack justifyContent="space-between" p={2}>
+    <HStack justifyContent="space-between" pl={2} pr={0} py={0}>
       <LuGlobe />
       <Heading size="sm">Daniel Lachica</Heading>
-      <LuMoon />
+      <ColorModeButton size="xs" borderRadius={0} />
     </HStack>
   );
 };
