0a5b284d5ede0b9ba6fe2a19d36824eae54437c2
Daniel Lachica <daniel.lachica82@gmail.com>
2026-07-21 22:10:18 +0800

    Add sticky navbar with glassmorphism effect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,32 @@
-import { HStack } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
 import { LuHash } from "react-icons/lu";
 import IconLink from "./IconLink";
 import Links from "@/data/links";
 
 const Navbar = () => {
   return (
-    <HStack gap={12} hideBelow="md" justifyContent="center" p={10}>
-      {Links.nav.map((link) => (
-        <IconLink
-          key={link}
-          icon={LuHash}
-          href={`#${link}`}
-          size="18px"
-          label={link}
-        />
-      ))}
-    </HStack>
+    <Box
+      as="header"
+      className="bg-glass"
+      pos="sticky"
+      top="0"
+      left="0"
+      right="0"
+      zIndex="sticky"
+      boxShadow="sm"
+    >
+      <HStack gap={12} hideBelow="md" justifyContent="center" p={10}>
+        {Links.nav.map((link) => (
+          <IconLink
+            key={link}
+            icon={LuHash}
+            href={`#${link}`}
+            size="18px"
+            label={link}
+          />
+        ))}
+      </HStack>
+    </Box>
   );
 };
 
