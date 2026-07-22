865d7a6d44c1daa2eef1b03c4d55ba0bf8f7e49a
Daniel Lachica <daniel.lachica82@gmail.com>
2026-07-22 21:03:32 +0800

    Add logos to skill tags

---
+++ src/assets/logos/index.ts
@@ -0,0 +1,71 @@
+/**
+ *
+ * All icons are from https://simpleicons.org/
+ * and https://worldvectorlogo.com/
+ *
+ */
+import react from "./react.svg";
+import bootstrap from "./bootstrap.svg";
+import javascript from "./javascript.svg";
+import typescript from "./typescript.svg";
+import vuejs from "./vuejs.svg";
+import inertia from "./inertia.svg";
+import html5 from "./htm5l.svg";
+import css from "./css.svg";
+import tailwindcss from "./tailwindcss.svg";
+import chakraui from "./chakraui.svg";
+import pwa from "./pwa.svg";
+import php from "./php.svg";
+import python from "./python.svg";
+import laravel from "./laravel.svg";
+import sonarqubeserver from "./sonarqubeserver.svg";
+import json from "./json.svg";
+import caspio from "./caspio.svg";
+import mysql from "./mysql.svg";
+import sqlite from "./sqlite.svg";
+import postgresql from "./postgresql.svg";
+import supabase from "./supabase.svg";
+import git from "./git.svg";
+import github from "./github.svg";
+import gitlab from "./gitlab.svg";
+import vercel from "./vercel.svg";
+import jira from "./jira.svg";
+import figma from "./figma.svg";
+import canva from "./canva.svg";
+import photoshop from "./photoshop.svg";
+import illustrator from "./illustrator.svg";
+import invision from "./invision.svg";
+
+export const logos = {
+  react,
+  bootstrap,
+  javascript,
+  typescript,
+  vuejs,
+  inertia,
+  html5,
+  css,
+  tailwindcss,
+  chakraui,
+  pwa,
+  php,
+  python,
+  laravel,
+  sonarqubeserver,
+  json,
+  caspio,
+  mysql,
+  sqlite,
+  postgresql,
+  supabase,
+  git,
+  github,
+  gitlab,
+  vercel,
+  jira,
+  figma,
+  canva,
+  photoshop,
+  illustrator,
+  invision,
+};
--- src/components/SkillTag.tsx
+++ src/components/SkillTag.tsx
@@ -4,8 +4,8 @@ import { Badge, Image, Text } from "@chakra-ui/react";
 const SkillTag = ({ label, logo }: Skill) => {
   return (
     <Badge size="lg" variant="surface">
-      {logo && <Image src={logo} height="18px" />}
-      <Text textTransform="capitalize">{label}</Text>
+      {logo && <Image src={logo} height="16px" width="16px" mr={1} />}
+      <Text>{label}</Text>
     </Badge>
   );
 };
