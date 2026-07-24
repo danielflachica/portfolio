3f7823e26c9465b26fce53f924659580b032f162
Daniel Lachica <daniel.lachica82@gmail.com>
2026-07-24 13:31:44 +0800

    Add Tooltip to IconLink component

--- src/components/IconLink.tsx
+++ src/components/IconLink.tsx
@@ -1,4 +1,5 @@
 import { HStack, Text } from "@chakra-ui/react";
+import { Tooltip } from "@/components/ui/tooltip";
 import type { IconType } from "react-icons";
 
 interface Props {
@@ -7,6 +8,7 @@ interface Props {
   label?: string;
   size?: string;
   target?: "_self" | "_blank" | "_parent" | "_top";
+  tooltip?: string;
 }
 
 const IconLink = ({
@@ -15,18 +17,21 @@ const IconLink = ({
   label,
   size = "16px",
   target = "_self",
+  tooltip,
 }: Props) => {
   return (
-    <a href={href} target={target}>
-      <HStack>
-        <IconComponent aria-hidden="true" fontSize={size} />
-        {label && (
-          <Text textTransform="capitalize" fontSize={size}>
-            {label}
-          </Text>
-        )}
-      </HStack>
-    </a>
+    <Tooltip content={tooltip} disabled={!tooltip || tooltip === ""} showArrow>
+      <a href={href} target={target}>
+        <HStack>
+          <IconComponent aria-hidden="true" size={size} />
+          {label && (
+            <Text textTransform="capitalize" fontSize={size}>
+              {label}
+            </Text>
+          )}
+        </HStack>
+      </a>
+    </Tooltip>
   );
 };
 
