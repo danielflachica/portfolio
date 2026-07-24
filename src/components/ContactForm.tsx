40fae03fb7a5a3cafe840d3d63cc66e270685c29
Daniel Lachica <daniel.lachica82@gmail.com>
2026-07-24 14:18:59 +0800

    Add ContactForm component

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,59 @@
+import { Button, Card, Field, Input, Stack, Textarea } from "@chakra-ui/react";
+
+const ContactForm = () => {
+  return (
+    <form>
+      <Card.Root maxW="lg" mx="auto">
+        <Card.Header>
+          <Card.Title>Contact Me</Card.Title>
+          <Card.Description>
+            Fill out the form below to send me an email
+          </Card.Description>
+        </Card.Header>
+        <Card.Body>
+          <Stack gap="4" w="full">
+            <Field.Root required>
+              <Field.Label>
+                Name <Field.RequiredIndicator />
+              </Field.Label>
+              <Input placeholder="What's your name?" />
+              <Field.ErrorText>This field is required</Field.ErrorText>
+            </Field.Root>
+            <Field.Root required>
+              <Field.Label>
+                Email <Field.RequiredIndicator />
+              </Field.Label>
+              <Input placeholder="e.g. juandelacruz@gmail.com" />
+              <Field.ErrorText>This field is required</Field.ErrorText>
+            </Field.Root>
+            <Field.Root required>
+              <Field.Label>
+                Subject <Field.RequiredIndicator />
+              </Field.Label>
+              <Input placeholder="What would you like to discuss?" />
+              <Field.ErrorText>This field is required</Field.ErrorText>
+            </Field.Root>
+            <Field.Root required>
+              <Field.Label>
+                Message <Field.RequiredIndicator />
+              </Field.Label>
+              <Textarea
+                placeholder="Enter your message here"
+                autoresize
+                maxH="10lh"
+              />
+              <Field.ErrorText>This field is required</Field.ErrorText>
+            </Field.Root>
+          </Stack>
+        </Card.Body>
+        <Card.Footer>
+          <Button colorPalette="blue" mb={2} w="full" variant="solid">
+            Send Message
+          </Button>
+        </Card.Footer>
+      </Card.Root>
+    </form>
+  );
+};
+
+export default ContactForm;
