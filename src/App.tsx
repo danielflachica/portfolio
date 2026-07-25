a431a21dd7a3ec013cc9a34b990a0462680ca98a
Daniel Lachica <daniel.lachica82@gmail.com>
2026-07-25 22:32:17 +0800

    Extract measurements to layout file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
-import Footer from "./components/Footer";
+import { HERO_HEIGHT, NAVBAR_HEIGHT, SCREEN_HEIGHT } from "./constants/layout";
+import Topbar from "./components/Topbar";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Section from "./components/Section";
 import Hero from "./components/sections/Hero";
 import About from "./components/sections/About";
@@ -11,13 +13,8 @@ import Skills from "./components/sections/Skills";
 import Projects from "./components/sections/Projects";
 import Contact from "./components/sections/Contact";
 import useSmoothScroll from "./hooks/useSmoothScroll";
-import Topbar from "./components/Topbar";
 
 function App() {
-  const screenHeight = "100dvh";
-  const navHeight = "100px";
-  const heroHeight = `calc(${screenHeight} - ${navHeight})`;
-
   useSmoothScroll();
 
   return (
@@ -34,15 +31,15 @@ function App() {
         </GridItem>
         <GridItem area="main">
           <Hero
-            height={{ base: screenHeight, md: heroHeight }}
+            height={{ base: SCREEN_HEIGHT, md: HERO_HEIGHT }}
             bg="bg.emphasized"
           />
           <Navbar
-            height={navHeight}
-            mt={{ base: 0, md: heroHeight }}
+            height={NAVBAR_HEIGHT}
+            mt={{ base: 0, md: HERO_HEIGHT }}
             hideBelow="md"
           />
-          <Section name="about" index={1} mt={{ base: screenHeight, md: 0 }}>
+          <Section name="about" index={1} mt={{ base: SCREEN_HEIGHT, md: 0 }}>
             <About />
           </Section>
           <Section name="experience" index={2} title="Work Experience">
---
+++ src/constants/layout.ts
@@ -0,0 +1,6 @@
+/*
+ * Default app layout constants
+ */
+export const SCREEN_HEIGHT = "100dvh";
+export const NAVBAR_HEIGHT = "100px";
+export const HERO_HEIGHT = `calc(${SCREEN_HEIGHT} - ${NAVBAR_HEIGHT})`;
