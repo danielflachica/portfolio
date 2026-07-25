48779f593551334d29e15e150b3fc57e02dcfcbb
Daniel Lachica <daniel.lachica82@gmail.com>
2026-07-25 18:40:50 +0800

    Update Footer z-index

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,14 @@ import Links from "@/data/links";
 import Middot from "./Middot";
 
 const Footer = () => (
-  <HStack color="fg.subtle" justifyContent="center" p={2}>
+  <HStack
+    color="fg.subtle"
+    bg="bg"
+    justifyContent="center"
+    p={2}
+    pos="relative"
+    zIndex="1"
+  >
     <Text fontSize="sm">&copy; {new Date().getFullYear()}</Text>
     <a href={Links.github.project} target="_blank">
       <Text fontSize="sm">&lt;{Me.username}&nbsp;/&gt;</Text>
