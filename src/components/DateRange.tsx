9c179b545f39f60864f265b350e4193f0312a1bb
Daniel Lachica <daniel.lachica82@gmail.com>
2026-07-26 23:53:07 +0800

    Make end prop optional on DateRange component

--- src/components/DateRange.tsx
+++ src/components/DateRange.tsx
@@ -3,15 +3,17 @@ import { LuChevronRight } from "react-icons/lu";
 
 interface Props {
   start: string;
-  end: string;
+  end?: string;
 }
 
 const DateRange = ({ start, end }: Props) => {
+  const showEnd = end && start !== end;
+
   return (
     <Flex as="span" display="inline-flex" alignItems="center" gap={1}>
       <Text as="span">{start}</Text>
-      <LuChevronRight />
-      <Text as="span">{end}</Text>
+      {showEnd && <LuChevronRight />}
+      {showEnd && <Text as="span">{end}</Text>}
     </Flex>
   );
 };
